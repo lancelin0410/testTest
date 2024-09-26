a95ccc5b118d14418b84a8e7bfaca8d2175b3659
lancelin <lancelin0410@gmail.com>
2024-09-26 19:47:03 +0800

    update

---
+++ assets/home-UQIrqPPz.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-CbLyf5FI.js";const n=r=>t({url:"/banner/list",method:"POST",data:r});export{n as g};
---
+++ assets/home-legacy-CeELcfpq.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-Ba2lXlG9.js"],(function(e,t){"use strict";var r;return{setters:[e=>{r=e.r}],execute:function(){e("g",(e=>r({url:"/banner/list",method:"POST",data:e})))}}}));
