a2855265bcef039927258147893d91027066044b
lancelin <lancelin0410@gmail.com>
2024-09-27 14:13:01 +0800

    update

--- assets/home-UQIrqPPz.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index-CbLyf5FI.js";const n=r=>t({url:"/banner/list",method:"POST",data:r});export{n as g};
--- assets/home-legacy-CeELcfpq.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy-Ba2lXlG9.js"],(function(e,t){"use strict";var r;return{setters:[e=>{r=e.r}],execute:function(){e("g",(e=>r({url:"/banner/list",method:"POST",data:e})))}}}));
